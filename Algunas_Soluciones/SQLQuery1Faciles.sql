a827a02f425281438bfbceb958c507436477f992
JohnIbarraBosonit <johnerik.ibarra@bosonit.com>
2021-08-04 19:20:25 +0200

    Create SQLQuery1Faciles.sql

---
+++ Algunas_Soluciones/SQLQuery1Faciles.sql
@@ -0,0 +1,8 @@
+SELECT CustomerID, MiddleName, LastName, CompanyName
+FROM SalesLT.Customer;
+
+SELECT Name, ProductNumber, Color 
+FROM SalesLT.Product;
+
+SELECT CustomerID, SalesOrderID
+FROM SalesLT.SalesOrderHeader;
