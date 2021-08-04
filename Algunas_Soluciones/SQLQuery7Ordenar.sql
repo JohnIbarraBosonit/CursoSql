a53023698b5ac14dbbae616a57a3f6be54f12515
JohnIbarraBosonit <johnerik.ibarra@bosonit.com>
2021-08-04 19:22:33 +0200

    Create SQLQuery7Ordenar.sql

---
+++ Algunas_Soluciones/SQLQuery7Ordenar.sql
@@ -0,0 +1,7 @@
+SELECT BusinessEntityID, LastName, FirstName, MiddleName
+FROM Person.Person
+ORDER BY LastName, FirstName, MiddleName ASC;
+
+SELECT BusinessEntityID, LastName, FirstName, MiddleName
+FROM Person.Person
+ORDER BY LastName, FirstName, MiddleName DESC;
